ff4dd18d181ab13483f62f435607bf8bc54af7f2
Cai <35024963+YESAndy@users.noreply.github.com>
2020-03-24 13:09:15 -0400

    Update requestGELDT.py

--- requestGELDT.py
+++ requestGELDT.py
@@ -6,11 +6,13 @@ import re
 import csv
 
 
+# get data
 def get_data(url):
     response = requests.get(url)
 
     if response.status_code == 200:
         print("response successfully")
+        
         try:
             json_content = json.loads(response.content)
             try:
@@ -19,23 +21,26 @@ def get_data(url):
                 data = timeline[0]['data']
                 print(data)
                 return data
+            # don't know what error..
             except KeyError:
                 print(json_content)
                 return None
+        # can not receive a valid json file
         except ValueError:
             print("fail to load {}'s data".format(name))
             return None
 
 
-
-
-country_file = open("country_list.txt", 'r')
+# set up output file
 outputfilename = "output.csv"
 summaryfilename = "summary.csv"
 outputfile = open(outputfilename, 'w', newline='')
 summaryfile = open(summaryfilename, 'w', newline='')
 output_writer = csv.writer(outputfile, delimiter=",")
 summary_writer = csv.writer(summaryfile, delimiter=",")
+
+# get country name
+country_file = open("country_list.txt", 'r')
 country_list = []
 countryName = []
 for row in country_file:
@@ -46,17 +51,17 @@ for row in country_file:
     countryName.append(row.strip()[3:])
 
 # read tone data
-data_tone = {}
-
 output_writer.writerow(["date_vol", "date_tone", "averge volume", "average tone"])
 for country, name in zip(country_list, countryName):
     averagetone = 0
     totalvol = 0
+    # for every country
     output_writer.writerow([name, " ", " ", " "])
     url_tone = "https://api.gdeltproject.org/api/v2/doc/doc?query=%22belt%20and%20road%22%20%22rules%22%20sourcecountry:{}&mode=timelinetone&startdatetime=20200101000000&enddatetime=20200323000000&sort=datedesc&format=JSON".format(country)
     url_vol = "https://api.gdeltproject.org/api/v2/doc/doc?query=%22belt%20and%20road%22%20%22rules%22%20sourcecountry:{}&mode=timelinevolraw&startdatetime=20200101000000&enddatetime=20200323000000&sort=datedesc&format=JSON".format(country)
     data_tone = get_data(url_tone)
     data_vol = get_data(url_vol)
+    # calculate and write their total volumes and average tones
     if data_tone and data_vol:
         for tone, vol in zip(data_tone, data_vol):
             output_writer.writerow([vol['date'], tone['date'], vol['value'], tone['value']])
@@ -64,14 +69,8 @@ for country, name in zip(country_list, countryName):
             totalvol += float(vol['value'])
         averagetone /= totalvol
         summary_writer.writerow([name, str(totalvol), str(averagetone)])
-    else:
+    else:# missing value
         summary_writer.writerow([name, "missing", "missing"])
 
-
-
-
-
-
-# response_json = response.json()
 
 
